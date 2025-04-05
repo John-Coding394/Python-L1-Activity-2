5ca01ce7479958c3bc36524f472168130879bbf7
John-Coding394 <johnchaghouri394@gmail.com>
2025-04-05 10:38:51 -0400

    Second commit

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ num=int(input("Enter a first number "))
 
 num1=int(input("Enter a second number "))
 sum=num+num1
+mul=num*num1
+sub=num-num1
+div=num/num1
 print("Addition of two numbers are ", sum)
+print("Multiplication of two numbers are ", mul) 
+print("Division of two numbers are ", div) 
+print("Subtraction of two numbers are ", sub) 
