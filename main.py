9bdfb11df076f1ff6ea20618f4cde1a16f8c0c75
John-Coding394 <johnchaghouri394@gmail.com>
2025-04-05 10:32:30 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("Write a python program to add two numbers ")
+num=int(input("Enter a first number "))
+
+num1=int(input("Enter a second number "))
+sum=num+num1
+print("Addition of two numbers are ", sum)
